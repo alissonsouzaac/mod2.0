e5246a71d551868476168c1841c05c3dcb135500
alissonsouzaac <42471999+alissonsouzaac@users.noreply.github.com>
2020-12-15 17:02:19 -0300

    Autenticação do úsuario pelo token

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,17 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionsController from './app/controllers/SessionsController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 
 routes.post('/user', UserController.store);
 routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/user', UserController.update);
 
 /*routes.get('/', async (req, res) => {
   const user = await User.create({
