ed7555e3efa14334412b7ae938d71fffa1c70300
alissonsouzaac <42471999+alissonsouzaac@users.noreply.github.com>
2020-12-15 03:29:43 -0300

    APIREST 2.0
    Aplicação que adiciona usuarios no back-end nodejs

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import User from './app/models/User';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
+
+routes.post('/user', UserController.store);
+
+/*routes.get('/', async (req, res) => {
   const user = await User.create({
     name: 'Alisson Costa',
     email: 'alisson@hotmail.com',
@@ -11,6 +16,6 @@ routes.get('/', async (req, res) => {
   });
 
     return res.json(user);
-});
+});*/
 
 export default routes;
