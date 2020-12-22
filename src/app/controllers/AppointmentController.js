cc49a326eb49b60bb2ed08bef9642a371dceeb79
alissonsouzaac <42471999+alissonsouzaac@users.noreply.github.com>
2020-12-22 16:39:15 -0300

    Templates de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -118,6 +118,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ]
     });
 
@@ -140,7 +145,16 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: ' Agendamento cancelado',
-      text: 'Cancelamento de serviço',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          appointment.date,
+          "'dia' dd 'de' MMMM', ás' H:mm'h'",
+          { locale: pt }
+          )
+      },
     })
 
     return res.json(appointment);
